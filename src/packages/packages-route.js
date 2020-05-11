66c4fc7c5ce310b250bb50645bf33026ccca4903
Maleckim <maleckimatthewm@gmail.com>
2020-05-11 18:10:11 -0500

    add package endpoint functional

--- src/packages/packages-route.js
+++ src/packages/packages-route.js
@@ -4,21 +4,13 @@ const packagesService = require('./packages-service');
 const packagesRouter = express.Router();
 const jsonBodyParser = express.json();
 
-packagesRouter.route('/').get((req, res, next) => {
+packagesRouter.route('/').post(jsonBodyParser, (req, res, next) => {
+  const { collectionId, name } = req.body;
   packagesService
-    .getAllPackages(req.app.get('db'))
-    .then((all_packages) => {
-      res.status(200).json(all_packages);
-    })
-    .catch(next);
+    .addPackage(req.app.get('db'), collectionId, name)
+    .then((addedPack) => {
+      res.status(200).json(addedPack);
+    });
 });
-
-// packagesRouter.route('/collection').get((req, res, next) => {
-//   let collectionId = req.query.collectionId;
-
-//   packagesService
-//     .collectionSpecificPackages(req.app.get('db'), collectionId)
-//     .then();
-// });
 
 module.exports = packagesRouter;
