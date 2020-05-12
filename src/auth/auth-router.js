3e370d2cd8b1960ac5f63809dcbc4a115bcb933c
Maleckim <maleckimatthewm@gmail.com>
2020-05-11 21:55:55 -0500

    added if exists?

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -4,39 +4,51 @@ const AuthService = require('./auth-service');
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
-authRouter.post('/login', jsonBodyParser, (req, res, next) => {
-  const { email, password } = req.body;
-  const loginuser = { email, password };
+authRouter
+  .get('/login', (req, res, next) => {
+    let { email } = req.query;
 
-  for (const [key, value] of Object.entries(loginuser))
-    if (value == null)
-      return res.status(400).json({
-        error: `Missing ${key} in request body`,
-      });
-
-  AuthService.getUserWithUserName(req.app.get('db'), loginuser.email)
-    .then((dbUser) => {
-      if (!dbUser) {
-        return res
-          .status(400)
-          .json({ error: 'Inccorect user_name or password' });
+    AuthService.getUserWithUserName(req.app.get('db'), email).then((exists) => {
+      if (exists) {
+        res.send(true);
+      } else {
+        res.send(false);
       }
-      return AuthService.comparePasswords(
-        loginuser.password,
-        dbUser.password
-      ).then((compareMatch) => {
-        if (!compareMatch) {
+    });
+  })
+  .post('/login', jsonBodyParser, (req, res, next) => {
+    const { email, password } = req.body;
+    const loginuser = { email, password };
+
+    for (const [key, value] of Object.entries(loginuser))
+      if (value == null)
+        return res.status(400).json({
+          error: `Missing ${key} in request body`,
+        });
+
+    AuthService.getUserWithUserName(req.app.get('db'), loginuser.email)
+      .then((dbUser) => {
+        if (!dbUser) {
           return res
             .status(400)
-            .json({ error: 'Incorrect user_name or password' });
+            .json({ error: 'Inccorect user_name or password' });
         }
+        return AuthService.comparePasswords(
+          loginuser.password,
+          dbUser.password
+        ).then((compareMatch) => {
+          if (!compareMatch) {
+            return res
+              .status(400)
+              .json({ error: 'Incorrect user_name or password' });
+          }
 
-        const sub = `${dbUser.id}`;
-        const payload = { email: dbUser.email };
-        res.send({ authToken: AuthService.createJwt(sub, payload) });
-      });
-    })
-    .catch(next);
-});
+          const sub = `${dbUser.id}`;
+          const payload = { email: dbUser.email };
+          res.send({ authToken: AuthService.createJwt(sub, payload) });
+        });
+      })
+      .catch(next);
+  });
 
 module.exports = authRouter;
