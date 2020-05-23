862953b9f6b22dd211a36179a7b9a808ccfad84d
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-23 12:45:16 -0700

    changed again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      script: './src/server.js',
+      script: './src/app.js',
       instances: 'max',
       env: {
         NODE_ENV: 'development',
