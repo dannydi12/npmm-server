3be2083f658eed573546facd766b5c2e4ab750d2
Mart <maleckimatthewm@gmail.com>
2020-06-06 18:01:09 -0500

    configured a starting point for reset password email

--- src/users/users-route.js
+++ src/users/users-route.js
@@ -3,6 +3,7 @@ const usersService = require('./users-service');
 const collectionService = require('../collections/collections-service');
 const packageService = require('../packages/packages-service');
 const AuthService = require('../auth/auth-service');
+const emailService = require('./password-mailer');
 const bcrypt = require('bcryptjs');
 
 const usersRouter = express.Router();
@@ -10,9 +11,22 @@ const jsonBodyParser = express.json();
 
 usersRouter.route('/').post(jsonBodyParser, (req, res, next) => {
   let { email, password } = req.body;
+  let { retrieval } = req.query;
+
+  if (retrieval === 'true') {
+    return AuthService.getUserWithUserName(req.app.get('db'), email)
+      .then((user) => {
+        return emailService(user.email, user.password).then((sent) => {
+          return res.status(200).json({ success: 'Password Reset Link' });
+        });
+      })
+      .catch(next);
+  }
+
   if (!email || !password) {
     return res.status(400).json({ error: 'Missing required field' });
   }
+
   let saltedPass = bcrypt.hashSync(password, 11);
 
   let sub;
@@ -39,5 +53,19 @@ usersRouter.route('/').post(jsonBodyParser, (req, res, next) => {
     })
     .catch(next);
 });
+usersRouter
+  .route('/password-retrieval')
+  .post(jsonBodyParser, (req, res, next) => {
+    const { email } = req.body;
+
+    AuthService.getUserWithUserName(req.app.get('db'), email)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({ working: 'user does not exist' });
+        }
+        return res.status(200).send({ working: 'working' });
+      })
+      .catch(next);
+  });
 
 module.exports = usersRouter;
